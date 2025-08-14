a3d1e68d653e2393c7f3cefd00e29cdc70c17a0d
Carlos Eduardo Barcelos <carlosebarcelos24@gmail.com>
2025-08-14 15:59:41 -0300

    Create exercicio01.c
    Mudança de valor de uma variável por meio de ponteiros

---
+++ aula01/atividade01/exercicio01.c
@@ -0,0 +1,24 @@
+// Exercício 1 - APC-II
+// Carlos Eduardo Sousa Barcelos - 2512130003
+
+#include <stdio.h>
+
+int main(){
+    int i =5;
+    int *p;
+    p = &i;
+
+    printf("&p: %p\n", &p);
+    printf("p (&i - endereço de i): %p\n", p);
+    printf("*p (i - valor de i): %d\n", *p);
+
+    *p = 7;
+
+    printf("\n");
+    printf("&p: %p\n", &p);
+    printf("p (&i - endereço de i): %p\n", p);
+    printf("*p (i - valor de i): %d\n", *p);
+
+
+    return 0;
+}
